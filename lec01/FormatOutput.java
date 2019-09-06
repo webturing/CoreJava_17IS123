51534cbeb88f00ee1e3a5e5185c6fde9c3ead8cf
ZHAO Jing <ceo@webturing.com>
2019-09-06 18:12:06 +0800

    Add files via upload

---
+++ lec01/FormatOutput.java
@@ -0,0 +1,7 @@
+public class FormatOutput {
+	public static void main(String[] args) {
+		System.out.print("hello world\n");
+		System.out.println("hello world");
+		System.out.printf("%.2f",7.0/5);//1.40
+	}
+}
---
+++ lec01/P1002.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class P1002 {
+	public static void main(String[] args) {
+		Scanner cin = new Scanner(System.in);
+		int a = cin.nextInt();
+		int b = cin.nextInt();
+		int c = cin.nextInt();
+		double x1, x2;
+		double delta = Math.sqrt(b * b - 4 * a * c);
+		x1 = (-b - delta) / (2 * a);
+		x2 = (-b + delta) / (2 * a);
+		if (x1 < x2) {
+			double t = x1;
+			x1 = x2;
+			x2 = t;
+		}
+		System.out.printf("%.2f %.2f", x1, x2);
+	}
+}
