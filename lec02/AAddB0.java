4d0a6ab5b1a1f20c0770f3a48652804feb454b38
ZHAO Jing <ceo@webturing.com>
2019-09-07 16:21:00 +0800

    Add files via upload

---
+++ lec02/AAddB0.java
@@ -0,0 +1,15 @@
+package io;
+
+public class AAddB0 {
+
+	/**
+	 *0×éÊäÈë
+	 */
+	public static void main(String[] args) {
+		int a=123;
+		int b=456;
+		System.out.println(a+b);
+
+	}
+
+}
---
+++ lec02/AAddB1.java
@@ -0,0 +1,18 @@
+package io;
+
+import java.util.Scanner;
+
+public class AAddB1 {
+
+	/**
+	 *1×éÊäÈë
+	 */
+	public static void main(String[] args) {
+		Scanner cin=new Scanner(System.in);
+		int a=cin.nextInt();
+		int b=cin.nextInt();
+		System.out.println(a+b);
+
+	}
+
+}
