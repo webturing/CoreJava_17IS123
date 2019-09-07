80954025b0a99d60f06927fc5dbfd98c0ca3bbb2
ZHAO Jing <ceo@webturing.com>
2019-09-07 16:20:32 +0800

    Add files via upload

---
+++ lec02/IntegerDemo.java
@@ -0,0 +1,17 @@
+
+public class IntegerDemo {
+
+	public static void main(String[] args) {
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);//2^31-1 21вк
+		System.out.println(Integer.toBinaryString(1023));
+		System.out.println(Integer.toBinaryString(-1));
+		System.out.println(Integer.toString(1023, 2));
+		String s="111";
+		System.out.println(Integer.parseInt(s,2));//7
+		
+		System.out.println(Integer.parseInt(s,16));//256+16+1
+
+	}
+
+}
