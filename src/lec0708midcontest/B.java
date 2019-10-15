797805c29571139d39d0b340f715acecf916e2ec
webturing <zj@webturing.com>
2019-10-15 10:15:39 +0800

    B小妖怪暗恋: 字符串，注意题目下标从1开始计数

---
+++ src/lec0708midcontest/B.java
@@ -0,0 +1,22 @@
+package lec0708midcontest;
+
+import java.util.Scanner;
+
+public class B {
+    public static void main(String[] args) {
+        for (int T = cin.nextInt(); T-- > 0; ) {
+            int p = cin.nextInt();
+            String s = cin.next();
+            System.err.println("p=" + p + ",s=" + s);
+            if (p > s.length()) {
+                System.out.println("single dog!");
+            } else {
+                System.out.println(s.charAt(p - 1));
+            }
+        }
+        cin.close();
+    }
+
+    static Scanner cin = new Scanner(System.in);
+}
+
