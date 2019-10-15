b7196e39a1e71a1f3650cbd0de457c0cac481c7b
webturing <zj@webturing.com>
2019-10-15 10:34:12 +0800

    E:字符串compareTo

---
+++ src/lec0708midcontest/E.java
@@ -0,0 +1,11 @@
+package lec0708midcontest;
+
+import java.util.Scanner;
+ 
+public class E {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        System.out.println(cin.next().compareTo(cin.next()));
+        cin.close();
+    }
+}
